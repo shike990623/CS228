f572c16b3b20b1ee54ce69af5439a7b34e23549d
shike990623 <59883822+shike990623@users.noreply.github.com>
2020-09-05 22:37:09 -0400

    Update leapInfiniteLoop.js

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,9 +1,12 @@
 var controllerOptions = {};
 var i = 0;
-
+var x = window.innerWidth/2;
+var y = window.innerHeight/2;
 Leap.loop(controllerOptions, function(frame)
 {
+clear()
 console.log(i)
+circle(x + (Math.floor(Math.random()* 2) - 1),y + (Math.floor(Math.random()* 2) - 1),100)
 
 }
 );
