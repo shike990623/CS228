4ce42dd3bfa75aa593eb4a66641fac8dbf377c6c
shike990623 <59883822+shike990623@users.noreply.github.com>
2020-09-19 23:05:12 -0400

    1

--- Record.js
+++ Record.js
@@ -4,19 +4,32 @@ var rawXMin = -200;
 var rawXMax = 200;
 var rawYMin = 20;
 var rawYMax = 200;
+var previousNumHands = 0;
+var currentNumHands = 0;
+var twoHands;
 
 
 Leap.loop(controllerOptions, function(frame){
     clear();
     HandleFrame(frame);
+        currentNumHands = frame.hands.length;
+        //console.log("Prev: " + previousNumHands + " -- Curr: " + currentNumHands);
+        previousNumHands = currentNumHands;
 }
-);
+)
 
 
 function HandleFrame(frame) {
     if(frame.hands.length == 1){
+        twoHands = false;
         var hand = frame.hands[0];
-        HandleHand(hand);
+        HandleHand(hand, twoHands);
+    } else if (frame.hands.length > 1) {
+        twoHands = true;
+        var hand = frame.hands[0];
+        HandleHand(hand,twoHands);
+    } else {
+        twoHands = false;
     }
 }
 
@@ -27,7 +40,7 @@ function HandleHand(hand) {
     for (var b2=0; b2<4; b2++){
         for(var b3=0; b3<fingers.length; b3++){
             var bones = fingers[b3].bones;
-            HandleBone(bones[b1]);
+            HandleBone(bones[b1],twoHands);
         }
         b1--;
     }
@@ -42,7 +55,7 @@ function HandleFinger(finger) {
     }
 }
 
-function HandleBone(bone){
+function HandleBone(bone,twoHands){
     var bone_end = bone.nextJoint;
     var endX = bone_end[0];
     var endY = bone_end[1];
@@ -56,18 +69,34 @@ function HandleBone(bone){
     var newBone_end = TransformCoordinates(endX,endZ-endY)
     var newBone_start = TransformCoordinates(startX,startZ-startY)
 
-    if (bone.type == 0){
-        strokeWeight(10);
-        stroke(200);
-    } else if (bone.type == 1){
-        strokeWeight(8);
-        stroke(150);
-    } else if (bone.type == 2){
-        strokeWeight(6);
-        stroke(100);
-    } else if (bone.type == 3){
-        strokeWeight(4);
-        stroke(50);
+    if (twoHands) {
+        if (bone.type == 0) {
+            strokeWeight(8);
+            stroke(250,0,0);
+        } else if (bone.type == 1) {
+            strokeWeight(6);
+            stroke(180,0,0);
+        } else if (bone.type == 2) {
+            strokeWeight(4);
+            stroke(120,0,0);
+        } else {
+            strokeWeight(2);
+            stroke(80,0,0);
+        }
+    } else {
+        if (bone.type == 0) {
+            strokeWeight(8);
+            stroke(0,250,0);
+        } else if (bone.type == 1) {
+            strokeWeight(6);
+            stroke(0,180,0);
+        } else if (bone.type == 2) {
+            strokeWeight(4);
+            stroke(0,120,0);
+        } else {
+            strokeWeight(2);
+            stroke(0,80,0);
+        }
     }
 
     line(newBone_end[0], newBone_end[1], newBone_start[0], newBone_start[1]);
