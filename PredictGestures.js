b1ab696ebbafe63d9f3e8412ddce51832cf5ba15
shike990623 <59883822+shike990623@users.noreply.github.com>
2020-10-12 01:23:34 -0400

    1

--- PredictGestures.js
+++ PredictGestures.js
@@ -1,4 +1,4 @@
-var testingSampleIndex = 0;
+//var testingSampleIndex = 0;
 const knnClassifier = ml5.KNNClassifier();
 
 var trainingCompleted = false;
@@ -15,8 +15,12 @@ var oneFrameOfData = nj.zeros([5, 4, 6]);
 var previousNumHands = 0;
 var currentNumHands = 0;
 var moreThanOneHand;
-var numSamples = 100;
+//var numSamples = 100;
 var currentSample = 0;
+var predictedClassLabels = nj.zeros(2);
+var n = 0;
+var m = 0;
+var d = 1;
 
 Leap.loop(controllerOptions, function(frame){
     clear();
@@ -33,6 +37,28 @@ Leap.loop(controllerOptions, function(frame){
 function Train(){
     trainingCompleted = true;
 
+    for (var i = 0; i < train0.shape[3]; i++) {
+        features = train0.pick(null,null,null,i);
+        features = features.reshape(120);
+        //console.log(features.toString());
+        knnClassifier.addExample(features.tolist(), 0);
+    }
+
+
+    for (var i = 0; i < train1.shape[3]; i++) {
+        features = train1.pick(null,null,null,i);
+        features = features.reshape(120);
+        //console.log(features.toString());
+        knnClassifier.addExample(features.tolist(), 1);
+    }
+
+    for (var i = 0; i < train2.shape[3]; i++) {
+        features = train2.pick(null,null,null,i);
+        features = features.reshape(120);
+        //console.log(features.toString());
+        knnClassifier.addExample(features.tolist(), 2);
+    }
+
     for (var i = 0; i < train3.shape[3]; i++) {
         features = train3.pick(null,null,null,i);
         features = features.reshape(120);
@@ -40,19 +66,132 @@ function Train(){
         knnClassifier.addExample(features.tolist(), 3);
     }
 
+    for (var i = 0; i < train4.shape[3]; i++) {
+        features = train4.pick(null,null,null,i);
+        features = features.reshape(120);
+        //console.log(features.toString());
+        knnClassifier.addExample(features.tolist(), 4);
+    }
+
     for (var i = 0; i < train5.shape[3]; i++) {
         features = train5.pick(null, null, null, i);
         features = features.reshape(120);
-        //console.log(features.toString());
         knnClassifier.addExample(features.tolist(), 5);
     }
+    for (var i = 0; i < train6.shape[3]; i++) {
+        features = train6.pick(null,null,null,i);
+        features = features.reshape(120);
+        knnClassifier.addExample(features.tolist(), 6);
+    }
+    for (var i = 0; i < train7.shape[3]; i++) {
+        features = train7.pick(null,null,null,i);
+        features = features.reshape(120);
+        knnClassifier.addExample(features.tolist(), 7);
+    }
+
+    for (var i = 0; i < train8.shape[3]; i++) {
+        features = train8.pick(null,null,null,i);
+        features = features.reshape(120);
+
+        knnClassifier.addExample(features.tolist(), 8);
+    }
+
+    for (var i = 0; i < train9.shape[3]; i++) {
+        features = train9.pick(null,null,null,i);
+        features = features.reshape(120);
+        knnClassifier.addExample(features.tolist(), 9);
+    }
+
+
+
+
+
+}
+
+function CenterData(){
+    var xValues = oneFrameOfData.slice([],[],[0,6,3]);
+
+    var currentMean = xValues.mean();
+
+    var horizontalShift = (0.5 - currentMean);
+
+    // console.log("before: " + currentMean);
+
+    for (var i = 0; i < 5; i++) {     // rows
+        for (var j = 0; j < 4; j++) {   // columns
+            // Top X
+            var currentX = oneFrameOfData.get(i, j, 0);
+
+            var shiftedX = currentX + horizontalShift;
+
+            oneFrameOfData.set(i, j, 0, shiftedX);
+
+            // Bottom X
+            currentX = oneFrameOfData.get(i, j, 3);
+
+            shiftedX = currentX + horizontalShift;
+
+            oneFrameOfData.set(i, j, 3, shiftedX);
+        }
+    }
+
+    var yValues = oneFrameOfData.slice([],[],[1,6,3]);
+
+    var YcurrentMean = yValues.mean();
+
+    var verticalShift = (0.5 - YcurrentMean);
+
+
+    for (var i = 0; i < 5; i++) {     // rows
+        for (var j = 0; j < 4; j++) {   // columns
+            // Top Y
+            var currentY = oneFrameOfData.get(i, j, 1);
+
+            var shiftedY = currentY + verticalShift;
+
+            oneFrameOfData.set(i, j, 1, shiftedY);
+
+            // Bottom Y
+            currentY = oneFrameOfData.get(i, j, 4);
+
+            shiftedY = currentY + verticalShift;
+
+            oneFrameOfData.set(i, j, 4, shiftedY);
+        }
+    }
+
+    var zValues = oneFrameOfData.slice([],[],[2,6,3]);
+
+    var ZcurrentMean = zValues.mean();
+
+    var ZShift = (0.5 - ZcurrentMean);
+
+
+    for (var i = 0; i < 5; i++) {     // rows
+        for (var j = 0; j < 4; j++) {   // columns
+            // Top Z
+            var currentZ = oneFrameOfData.get(i, j, 2);
+
+            var shiftedZ = currentZ + ZShift;
+
+            oneFrameOfData.set(i, j, 2, shiftedZ);
+
+            // Bottom Z
+            currentZ = oneFrameOfData.get(i, j, 5);
+
+            shiftedZ = currentZ + ZShift;
+
+            oneFrameOfData.set(i, j, 5, shiftedZ);
+        }
+    }
+
 
 }
 
 function Test(){
-    currentFeatures = oneFrameOfData.pick(null, null, null, testingSampleIndex);
+    CenterData();
+    currentFeatures = oneFrameOfData.pick(null, null, null, ).reshape(120);
 
-    currentLabel = 3;
 
     //console.log(currentFeatures.toString());
 
@@ -61,15 +200,13 @@ function Test(){
 }
 
 function GotResults(err, result){
-    console.log(testingSampleIndex + " : " + result.label);
+    var c = result.label;
 
-    testingSampleIndex++;
 
-    //console.log(test.shape[3]);
-
-    if (testingSampleIndex >= train3.shape[3]) {
-        testingSampleIndex = 0;
-    }
+    predictedClassLabels.set(parseInt(result.label));
+    n += 1;
+    m = (((n-1)*m) + (c == d))/n;
+    console.log(n + " " + m + " " + c);
 }
 
 function DrawCircles(){
